ade7a6086659572dc053cc52dd6e87d68cb21eba
Maxim <maximdeclient@gmail.com>
2023-11-22 14:01:05 +0100

    Add compass & register player death listener

--- src/main/java/com/maximde/onedayvaro/OneDayVaro.java
+++ src/main/java/com/maximde/onedayvaro/OneDayVaro.java
@@ -3,10 +3,7 @@ package com.maximde.onedayvaro;
 import com.maximde.onedayvaro.commands.Pause;
 import com.maximde.onedayvaro.commands.Resume;
 import com.maximde.onedayvaro.commands.Start;
-import com.maximde.onedayvaro.listeners.EntityDamageListener;
-import com.maximde.onedayvaro.listeners.PlayerInteractListener;
-import com.maximde.onedayvaro.listeners.PlayerJoinListener;
-import com.maximde.onedayvaro.listeners.PlayerMoveListener;
+import com.maximde.onedayvaro.listeners.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -31,6 +28,7 @@ public final class OneDayVaro extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new PlayerInteractListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerMoveListener(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerDeathListener(this), this);
     }
 
 }
--- src/main/java/com/maximde/onedayvaro/commands/Start.java
+++ src/main/java/com/maximde/onedayvaro/commands/Start.java
@@ -1,20 +1,25 @@
 package com.maximde.onedayvaro.commands;
 
 import com.maximde.onedayvaro.OneDayVaro;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Sound;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public record Start(OneDayVaro oneDayVaro) implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(!(sender instanceof Player player)) {
+            sender.sendMessage(ChatColor.RED + "You can only execute this command as a player!");
+            return false;
+        }
 
         if(!sender.isOp()) {
             sender.sendMessage(ChatColor.RED + "Keine Rechte!");
@@ -25,20 +30,62 @@ public record Start(OneDayVaro oneDayVaro) implements CommandExecutor {
             sender.sendMessage(ChatColor.RED + "Varo hat schon gestartet!");
             return false;
         }
+
         oneDayVaro.setPaused(false);
         Bukkit.broadcastMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "DAS VARO EVENT HAT BEGONNEN!");
-        for(Player player : Bukkit.getOnlinePlayers()) {
-            player.playSound(player.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 1F, 1F);
-            player.sendTitle(ChatColor.RED + "Renn um dein Leben!", "",  10, 50, 10);
-            player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 20 * 60, 255));
+
+        for(Player all : Bukkit.getOnlinePlayers()) {
+            all.playSound(all.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 1F, 1F);
+            all.sendTitle(ChatColor.RED + "Renn um dein Leben!", "",  10, 50, 10);
+            all.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 20 * 60, 255));
         }
-        if(sender instanceof Player player) {
-            player.getWorld().getWorldBorder().setCenter(player.getLocation().getX(), player.getLocation().getZ());
-            player.getWorld().getWorldBorder().setSize(1000, 5);
-            player.getWorld().setTime(0);
-            player.getWorld().setStorm(false);
-            player.getWorld().setHardcore(true);
-        }
+
+        player.getWorld().getWorldBorder().setCenter(player.getLocation().getX(), player.getLocation().getZ());
+        player.getWorld().getWorldBorder().setSize(1000, 5);
+        player.getWorld().setTime(0);
+        player.getWorld().setStorm(false);
+        player.getWorld().setHardcore(true);
+        Bukkit.getScheduler().runTaskLaterAsynchronously(oneDayVaro, this::giveCompass, (20L * 60) * 60);
         return false;
+    }
+
+    private void giveCompass() {
+        Bukkit.broadcastMessage(ChatColor.GOLD + "1h has passed and everyone got a compass!");
+
+        for(Player player : Bukkit.getOnlinePlayers()) {
+            var item = new ItemStack(Material.COMPASS);
+            item.getItemMeta().setDisplayName(ChatColor.RED + "Compass");
+            player.getInventory().addItem(item);
+        }
+
+        final BukkitRunnable repeatingTask = new BukkitRunnable() {
+            public void run() {
+                for(Player player : Bukkit.getOnlinePlayers()) {
+                    updateCompass();
+                }
+            }
+        };
+        repeatingTask.runTaskTimerAsynchronously(oneDayVaro, 20, 20 * 5);
+    }
+
+    private void updateCompass() {
+        for(Player player : Bukkit.getOnlinePlayers()) {
+            player.setCompassTarget(getNearest(player).getLocation());
+        }
+    }
+
+    private Player getNearest(Player player) {
+        double distance = Double.MAX_VALUE;
+        Player target = null;
+        for (Player all : Bukkit.getOnlinePlayers()) {
+            if(!all.getGameMode().equals(GameMode.SURVIVAL)) continue;
+            if(all == player) continue;
+            double dis = player.getLocation().distance(all.getLocation());
+            if (dis < distance) {
+                distance = dis;
+                target = all;
+            }
+        }
+        return target;
     }
 }
